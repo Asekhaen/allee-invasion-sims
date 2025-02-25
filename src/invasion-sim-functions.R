afbf53fe95a6e8b31012cdd0e096d4d7638ff1ff
Ben Phillips <ben.l.phillips@curtin.edu.au>
2025-02-25 08:14:34 +0800

    add functions file

---
+++ src/invasion-sim-functions.R
@@ -0,0 +1,133 @@
+# Functions for running the invasion sims under a genetic Allee effect
+
+# make the dispersal transition matrix
+make_transition_matrix <- function(x, m){
+  d <- matrix(0, nrow = x, ncol = x) # a dispersal transition matrix
+  m <- m
+  diag(d) <- 1-m
+  d[cbind(1:(x-1), 2:(x))] <- m/2
+  d[cbind(2:(x), 1:(x-1))] <- m/2
+  d[1,1] <- 1-m/2 # reflective boundaries
+  d[10,10] <- 1-m/2
+  d
+}
+
+state_vars <- function(){
+  N <- N.prime <- matrix(0, nrow = g, ncol = g) #columns are space, rows are time
+  p <- p.prime <- array(0, dim = c(g, g, n.l)) # space, time, loci
+  list(N=N, N.prime = N.prime, p = p, p.prime = p.prime)
+}
+
+# initialise the population
+initialise <- function(state){
+  state$N[1,1] <- 50
+  state$p[1, 1,] <- rbinom(n.l, 2*K, p.0)/(2*K) # initial founder effect
+  state
+}
+
+# takes population sizes and allele frequencies and returns offspring values
+reproduction <- function(state, t){
+  N.total <- sum(state$N[t, ])
+  if (N.total == 0) return(state)
+  # takes a time and returns realised number of females at that time
+  n_fem <- function(t) {
+    rbinom(n = g, size = state$N[t,], prob = 0.5)   
+  }
+  # takes a vector of N_t and returns number of offspring
+  breed <- function(t) {
+    n.f <- n_fem(t)
+    n.o <- n.f * r # make offspring
+  }
+  state$N.prime[t, ] <- breed(t) 
+  for (ll in 1:n.l){
+    state$p.prime[t, ,ll] <- rbinom(n = x, 
+                                    size = 2*state$N.prime[t, ], 
+                                    prob = state$p[t, ,ll])/(2*state$N.prime[t, ])
+    state$p.prime[t, ,ll] <- ifelse(!is.finite(state$p.prime[t, ,ll]), 0, state$p.prime[t, ,ll])
+  }
+  state
+}
+
+# kills homozygous offspring
+death <- function(state, t){
+  surv <- 1-state$p.prime[t, ,]^2 
+  surv.x <- apply(surv, 1, prod) # assuming independent assortment
+  #cat("survival ", surv.x, "\n")
+  state$N.prime[t, ] <- rbinom(x, state$N.prime[t, ], surv.x) #stochastic survival
+  state$N.prime[t, ] <- ifelse(state$N.prime[t, ] > K, K, state$N.prime[t, ]) # truncate at K
+  # cat("surviving ", state$N.prime[t, ], "\n")
+  state$p.prime[t, ,] <- (state$p.prime[t, ,]*(1-state$p.prime[t, ,]))/(1-state$p.prime[t, ,]^2) # deterministic reduction in p (selection)
+  state
+}
+
+dispersal <- function(state, t){
+  N.total <- sum(state$N.prime[t,])
+  if (N.total == 0) return(state)
+  #browser()
+  for (ll in 1:n.l){
+    # stochastic sample of p into the migrant pool
+    drift.sample <- rbinom(x, round(m*state$N.prime[t, ]), state$p.prime[t, ,ll])/round(m*state$N.prime[t, ])
+    #cat("\t drift sample: ", drift.sample, "\n")
+    drift.sample <- ifelse(!is.finite(drift.sample), 0, drift.sample)
+    state$p.prime[t, ,ll] <- (d %*% (state$N.prime[t, ] * drift.sample))/(d %*% state$N.prime[t,])
+    #cat("\t p sample: ", state$p.prime[t, ,ll], "\n")
+    state$p.prime[t, ,ll] <- ifelse(!is.finite(state$p.prime[t, ,ll]), 0, state$p.prime[t, ,ll])
+  }
+  state$N.prime[t,] <- d %*% state$N.prime[t,] #expected counts after dispersal
+  #state$N.prime[t,] <- rmultinom(n = 1, 
+  #                              size = N.total, 
+  #                              prob = state$N.prime[t,]/N.total) # make stochastic
+  
+  state$N[t+1,] <- round(state$N.prime[t,]) # pass it forward
+  state$p[t+1, ,] <- state$p.prime[t, ,]
+  state
+}
+
+# a function to pull them all together
+one_gen <- function(state, par, t){
+  state <- state |> 
+    reproduction(t) |>
+    death(t) |>
+    dispersal(t)
+  state
+}
+
+# to plot them at any given time
+plot_sim <- function(state){
+  matplot(1:g, t(state$N), 
+          type = "l", 
+          lty = 1, 
+          col = "grey50", 
+          bty = "l",
+          ylab = "Population size",
+          xlab = "Patch number")
+}
+
+all_gens <- function(par, plot = TRUE){
+  list2env(par, envir = parent.frame()) # make parameters available
+  states <- state_vars()
+  states <- initialise(states)
+  for (tt in 1:(par$g-1)){
+    states <- one_gen(state = states, par = par.list, t = tt)
+    #cat("state", states$N[tt,], "\n")
+  }
+  # the invasion over time
+  if (plot) plot_sim(states)
+  states
+}
+
+rep_runs <- function(par, reps = 10){
+  list2env(par, envir = parent.frame()) # make parameters available
+  extent <- vector(length = reps)
+  for (rr in 1:reps){
+    out <- all_gens(par, plot = FALSE)
+    ex.rep <- which(out$N[nrow(out$N),] > 0) # get extent
+    if (length(ex.rep)==0){
+      extent[rr] <- 0
+    } else {
+      extent[rr] <- max(which(out$N[nrow(out$N),] > 0))
+    }
+  }
+  extent[!is.finite(extent)] <- 0
+  extent
+}
