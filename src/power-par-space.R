1330a270ec16984c9faf46fe6be150ec7073a0f0
Ben Phillips <ben.l.phillips@curtin.edu.au>
2025-03-13 10:04:47 +0800

    Added power analysis varying sample size

--- src/power-par-space.R
+++ src/power-par-space.R
@@ -4,6 +4,8 @@
 # space being explored is m x r x K
 source("src/invasion-sim-functions.R")
 
+##### m, r, K #####
+
 # parameter space
 m.vec <- seq(0.05, 0.45, 0.05)
 r.vec <- seq(5, 50, 5)
@@ -71,3 +73,66 @@ ggplot(p.space, aes(x = m, y = r, fill = e.size)) +
   labs(title = "Effect size for varying K, r, and m -- n = 20") +
   theme_minimal()
 ggsave(filename = "out/e-size-k-n20.png")
+
+##### m, n, K #####
+
+m.vec <- seq(0.05, 0.45, 0.05)
+r.vec <- mean(c(24, 23, 13.5)) # from Izzy's experiment: dating + 1 day max growth in DF10 lines
+K.vec <- c(30, 50, 100)
+n.vec <- seq(10, 50, 10)
+
+p.space <- expand.grid(exp.reps = n.vec, m = m.vec, r = r.vec, K = K.vec)
+
+# other variables (kept constant)
+g <- x <- 10
+p.0 <- 0.05
+power.reps <- 100
+
+# loop through parameter space
+e.size <- matrix(nrow = nrow(p.space), ncol = power.reps)
+p.value <- matrix(nrow = nrow(p.space), ncol = power.reps)
+for (rr in 1:nrow(p.space)){
+  list2env(p.space[rr,], envir = environment()) # put current parameter space into global environment 
+  for (pp in 1:power.reps){ # run replicate experiments
+    n.l <- 40
+    rr.40 <- rep_runs(g = g, K = K, m = m, n.l = n.l, p.0 = p.0, r = r, x = x, reps = exp.reps)
+    n.l <- 10
+    rr.10 <- rep_runs(g = g, K = K, m = m, n.l = n.l, p.0 = p.0, r = r, x = x, reps = exp.reps)
+    t.t <- try(t.test(rr.10, rr.40), silent = TRUE)
+    if (inherits(t.t, "try-error")) {
+      #cat("Error encountered at iteration", rr, "- Skipping\n")
+      next
+    }
+    p.value[rr, pp] <- t.t$p.value
+    e.size[rr, pp] <- mean(rr.40) - mean(rr.10)
+  }
+}
+
+
+# combine results
+p.space <- cbind(p.space, 
+                 e.size = -apply(e.size, 1, mean, na.rm = TRUE), # switch orientation for easier intuition
+                 p.value = apply(p.value, 1, mean, na.rm = TRUE),
+                 power = apply(p.value<0.05, 1, mean, na.rm = TRUE))
+
+
+
+library(ggplot2)
+# power at K = 50, n = 10
+ggplot(p.space, aes(x = exp.reps, y = power)) +
+  geom_line() +
+  facet_grid(rows = vars(m), cols = vars(K)) +
+  labs(title = "Power for varying K, and m") +
+  xlab("Number of replicate invasions") +
+  ylab("Power") +
+  theme_minimal()
+ggsave(filename = "out/power-k-m-var_n.png")
+
+ggplot(p.space, aes(x = exp.reps, y = e.size)) +
+  geom_line() +
+  facet_grid(rows = vars(m), cols = vars(K)) +
+  labs(title = "Effect size for varying K, and m") +
+  xlab("Number of replicate invasions") +
+  ylab("Effect size") +
+  theme_minimal()
+ggsave(filename = "out/esize-k-m-var_n.png")
